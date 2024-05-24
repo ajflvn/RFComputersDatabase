50a7921bda15216b2dc417bd4e2d0b039987ff04
ajflvn <albertojesus.florido@viewnext.com>
2024-05-24 13:13:36 +0200

    Update run-tests.bat

--- run-tests.bat
+++ run-tests.bat
@@ -1,9 +1,9 @@
 @echo off
 setlocal
-set browsers=edge
+set browsers=chrome firefox
 REM set browsers=chrome firefox edge
 for %%i in (%browsers%) do (
-    echo "Running %%i tests"
+    echo Running %%i tests
     robot --variable WEB_BROWSER:%%i -d Results\%%i Tests\ComputersDatabase.robot
 )
 endlocal
